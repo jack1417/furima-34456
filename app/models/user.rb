5d6b9e3f57f969a3437520f5f5564153645a6fa3
jack1417 <78401554+jack1417@users.noreply.github.com>
2021-02-23 17:03:18 +0900

    モデルの単体テストコードを書く

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,          presence: true
+  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
   validates :family_name,       presence: true
   validates :first_name,        presence: true
+  validates :family_name, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,  format:{with: /\A[ぁ-んァ-ン一-龥]/}
   validates :family_name_kana,  presence: true
   validates :first_name_kana,   presence: true
   validates :birth_day,         presence: true
