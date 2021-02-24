234a4da2c039ed4ba612bdd277c501899a7b7126
jack1417 <78401554+jack1417@users.noreply.github.com>
2021-02-24 15:15:57 +0900

    最終修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-          validates :nickname
-          validates :family_name
-          validates :first_name
-          validates :family_name_kana
-          validates :first_name_kana
-          validates :birth_day
+   validates :nickname
+   validates :birth_day
+   validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+     validates :family_name
+     validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+     validates :family_name_kana
+     validates :first_name_kana
+    end
   end
-  
-  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
-  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
 end
