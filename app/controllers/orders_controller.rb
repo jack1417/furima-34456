defedf720318a198a732d39972b68a5cf37ae10b
jack1417 <78401554+jack1417@users.noreply.github.com>
2021-03-09 19:26:54 +0900

    PEYJPの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
       @item = Item.find(params[:item_id])
       @order_address = OrderDestination.new(destination_params)
       if @order_address.valid?
+        pay_item
         @order_address.save
         redirect_to root_path
       else
@@ -23,6 +24,15 @@ class OrdersController < ApplicationController
     private
   
     def destination_params
-      params.require(:order_destination).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+      params.require(:order_destination).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+        Payjp::Charge.create(
+        amount: @item.price,  
+        card: destination_params[:token],   
+        currency: 'jpy'                 
+      )
     end
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,12 +1,14 @@
 class OrderDestination 
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:order_id,:post_code,:prefecture_id,:city,:address,:building_name,:phone_number
+  attr_accessor :user_id,:item_id,:order_id,:post_code,:prefecture_id,:city,:address,:building_name,:phone_number,:token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :address
     validates :city
+    validates :token
+    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
 
